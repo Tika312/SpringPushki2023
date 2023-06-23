5a96afa803fad39ee621f933bf19c7a0e3a727c8
Tilek Kanmuratov <Tikanmuratov@gmail.com>
2023-06-23 18:48:08 +0300

    added from stash1

--- src/test/java/git/QiwiPayment.java
+++ src/test/java/git/QiwiPayment.java
@@ -1,14 +1,9 @@
 package git;
 
 public class QiwiPayment {
-<<<<<<< Updated upstream
     public static void main(String[] args) {
-        System.out.println("Qiwi payment");
-=======
 
-    public void sendPayment(){
-        System.out.println("Sending payment");
->>>>>>> Stashed changes
+        System.out.println("Stash1");
     }
 }
 
