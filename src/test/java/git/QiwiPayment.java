2b81cdba0ecbe707126dcffc04228031b05ae164
Tilek Kanmuratov <Tikanmuratov@gmail.com>
2023-06-23 18:41:23 +0300

    all added

--- src/test/java/git/QiwiPayment.java
+++ src/test/java/git/QiwiPayment.java
@@ -1,7 +1,14 @@
 package git;
 
 public class QiwiPayment {
+<<<<<<< Updated upstream
     public static void main(String[] args) {
         System.out.println("Qiwi payment");
+=======
+
+    public void sendPayment(){
+        System.out.println("Sending payment");
+>>>>>>> Stashed changes
     }
 }
+
