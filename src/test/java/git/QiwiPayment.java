5bbbb599a63fda5ac7132219dca63f9988a6c624
Tilek Kanmuratov <Tikanmuratov@gmail.com>
2023-06-23 19:13:28 +0300

    added tag

--- src/test/java/git/QiwiPayment.java
+++ src/test/java/git/QiwiPayment.java
@@ -3,8 +3,7 @@ package git;
 public class QiwiPayment {
     public static void main(String[] args) {
 
-        System.out.println("Stash1");
-        System.out.println("stash2");
+        System.out.println("tag");
     }
 }
 
